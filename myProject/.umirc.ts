ed1c4d6bf287a4ab69de35e0758368f191561db6
wangfq <1415405435@qq.com>
2023-08-19 17:24:47 +0800

    feat: 配置请求代理

--- myProject/.umirc.ts
+++ myProject/.umirc.ts
@@ -6,9 +6,15 @@ export default defineConfig({
     type: 'none',
   },
   alias: {
-    utils: resolve(__dirname,'./src/utils'), // 别名映射
-    components: resolve(__dirname,'./src/components'), // 别名映射
-    common: resolve(__dirname,'./src/common'), // 别名映射
+    utils: resolve(__dirname, './src/utils'), // 别名映射
+    components: resolve(__dirname, './src/components'), // 别名映射
+    common: resolve(__dirname, './src/common'), // 别名映射
+  },
+  proxy: {
+    '/api': {
+      target: 'http:127.0.0.1/7001',
+      changeOrigin: true,
+    },
   },
   // routes: [
   //   { path: '/', component: '@/pages/index' },
